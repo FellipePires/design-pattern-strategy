7d4e9bb09d7d871df2ed0a1e20dedd703cf7ae65
Fellipe Thomas <fellipecab@gmail.com>
2019-09-28 12:02:53 -0300

    Add model classes

---
+++ src/main/java/br/com/fthomasvp/model/CreditCard.java
@@ -0,0 +1,28 @@
+package br.com.fthomasvp.model;
+
+import br.com.fthomasvp.controller.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditCard implements Payment {
+
+  private String name;
+  private String cardNumber;
+
+  @Override
+  public void pay(Double value) {
+    Double tax = 0.015;
+    Double total = value + (value * tax);
+
+    System.out.print("Name: " + this.name);
+    System.out.print("\tCard Number: " + this.cardNumber);
+    System.out.printf("\tTotal = %.2f \n", total);
+  }
+
+}
---
+++ src/main/java/br/com/fthomasvp/model/PayPal.java
@@ -0,0 +1,26 @@
+package br.com.fthomasvp.model;
+
+import br.com.fthomasvp.controller.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayPal implements Payment {
+
+  private String email;
+  private String password;
+
+  @Override
+  public void pay(Double value) {
+    Double total = value;
+
+    System.out.print("Email: " + this.email);
+    System.out.printf("\tTotal = %.2f \n", total);
+  }
+
+}
